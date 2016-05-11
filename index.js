c62d8e5c38cc7862e193ddf3e3defa0826199ea5
Josh Harvey <josh@lamassu.is>
2016-05-11 22:34:16 +0300

    support sending to any number

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Client = require('twilio')
+var R = require('ramda')
 
 var client
 var fromNumber
@@ -6,6 +7,9 @@ var toNumber
 
 exports.NAME = 'Twilio'
 exports.SUPPORTED_MODULES = []
+
+var BAD_NUMBER_CODES = [21201, 21202, 21211, 21214, 21216, 21217, 21219, 21408,
+  21610, 21612, 21614, 21608]
 
 exports.config = function config (_config) {
   var accountSid = _config.accountSid
@@ -17,15 +21,22 @@ exports.config = function config (_config) {
 
 exports.sendMessage = function sendMessage (rec) {
   var body = rec.sms.body
-
+  const _toNumber = rec.sms.toNumber || toNumber
   return new Promise(function (resolve, reject) {
     client.messages.create({
       body: body,
-      to: toNumber,
+      to: _toNumber,
       from: fromNumber
     }, function (err, message) {
-      if (err) return reject(new Error(err.message))
-      resolve()
+      if (!err) return resolve()
+
+      if (R.contains(err.code, BAD_NUMBER_CODES)) {
+        var badNumberError = new Error(err.message)
+        badNumberError.name = 'BadNumberError'
+        return reject(badNumberError)
+      }
+
+      return reject(new Error(err.message))
     })
   })
 }
